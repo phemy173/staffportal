7b108bb7b23ff8a90fe650c4ef21e8816671c5ed
phemy173 <duyile.valentine@gmail.com>
2025-09-03 12:15:40 +0100

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import styles from "./Footer.css"
 function Footer() {
+    
     return (
-        <div class="footer bg-success text-white">
+        <footer class="footer bg-success text-white">
+            <div className="row p-5">
+                <div className="col-sm-4 flex">
+                        <li className="nav-item" ><a  className="" href="#">Home</a></li>
+                        <li><a href="#">Departments</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Resources</a></li>
+                        <li><a href="#">Support</a></li>
+                </div>
+                <div className="col-sm-4">
+                    <h5>Contact Information</h5>
+                    <p>HR: +234 812 345 6789</p>
+                    <p>IT: +234 900 800 7000</p>
+                </div>
+                <div className="col-sm-4">
+                    <img src="../images/gsl-logo.png" alt="gsl-logo" className="img-fluid" style={{width:"3rem"}} />
+                    <p>&copy; 2025 Greensprings School. All rights reserved.</p>
+                </div>
+            </div>
             <p>Our footer section will be here</p>
-        </div>
+        </footer>
     )
 }
 export default Footer;
